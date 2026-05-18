513581be55c8d255b56cbecefd28d81240ebea37
han031121 <han031121@gmail.com>
2026-05-18 15:06:06 +0900

    Create BlockGenerator

---
+++ blockGenerator/src/ofApp.cpp
@@ -0,0 +1,204 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetEscapeQuitsApp(false);
+
+	ofBackground(0);
+
+	//set initial value
+	initializeDrawValue();
+	initializeBlockValue();
+	loadGuiSettings();
+
+	//drawing GUI
+	updateLayout();
+	guiSetListener();
+	guiSetScale();
+
+	//generate objects
+	block_data = std::make_unique<blockData>(block_count_1, block_count_2, max_r, max_c, max_h);
+	//draw_object = std::make_unique<drawObject>(block_data.get(), image_size, image_size);
+
+	//update label gui
+	blockSettingUpdate();
+	blockCurrentInfoUpdate();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	statusUpdate();
+
+	if (generate_request && frame_delayed) {
+		block_data->generateBlock();
+		draw_object = std::make_unique<drawObject>(block_data.get(), image_size, image_size);
+		blockCurrentInfoUpdate();
+		generate_request = false;
+		frame_delayed = false;
+	}
+
+	if (draw_object) {
+		drawObjectUpdate();
+		draw_object->render();
+	}
+
+	if (fix_light) {
+		light_degree_xz = cam_degree_xz;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	//draw image components
+	ofSetColor(220);
+	ofDrawRectangle(rect_image);
+
+	if (draw_object) {
+		float size = std::min(rect_image.getWidth(), rect_image.getHeight());
+		float x = (2 * rect_image.x + rect_image.getWidth() - size) / 2;
+		float y = (2 * rect_image.y + rect_image.getHeight() - size) / 2;
+		ofSetColor(255);
+		draw_object->drawFbo(x, y, size, size);
+	}
+
+	//draw gui components
+	if (gui_on) {
+		ofSetColor(50);
+		ofDrawRectangle(rect_block_gui);
+		ofDrawRectangle(rect_draw_gui);
+
+		ofNoFill();
+		ofSetLineWidth(3);
+		ofDrawRectangle(rect_status_block);
+		ofDrawRectangle(rect_status_draw);
+		ofFill();
+
+		ofSetColor(255);
+		drawStatus();
+		drawEtc();
+	}
+
+	frame_delayed
+		= true;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	//set block
+	if (key == 'n' || key == 'N') {
+		setBlockClicked();
+	}
+
+	//generate block
+	else if (key == 'g' || key == 'G') {
+		generateBlockClicked();
+	}
+
+	//save image
+	else if (key == 's' || key == 'S') {
+		saveImageClicked();
+	}
+
+	//draw reset
+	else if (key == 'r' || key == 'R') {
+		drawResetClicked();
+	}
+
+	//gui scale up
+	else if (key == '=' && gui_on) {
+		if (gui_scale < 8) {
+			gui_scale += 0.5;
+			guiSetScale();
+		}
+	}
+
+	//gui scale down
+	else if (key == '-' && gui_on) {
+		if (gui_scale > 1) {
+			gui_scale -= 0.5;
+			guiSetScale();
+		}
+	}
+
+	//gui toggle
+	else if (key == OF_KEY_TAB) {
+		gui_on = !gui_on;
+		updateLayout();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+	//isImageDrag이면 이미지 회전
+	if (is_image_drag)
+		imageMouseDragged(x, y);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+	if (checkMouseOnImage()) {
+		is_image_drag = true;
+		drag_start_mouse = { ofGetMouseX(), ofGetMouseY() };
+		prev_cam_degree_xz = cam_degree_xz;
+		prev_cam_degree_y = cam_degree_y;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+	is_image_drag = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scroll_x, float scroll_y) {
+	if (checkMouseOnImage()) {
+		float coeff = 0.1f;
+
+		magnification += scroll_y * coeff;
+
+		if (magnification > 5)
+			magnification = 5;
+		if (magnification < 0.2)
+			magnification = 0.2;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+	updateLayout();
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {
+	saveGuiSettings();
+}
---
+++ blockGenerator/src/ofApp.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "ofMain.h"
+#include "blockData.h"
+#include "drawObject.h"
+
+#define MARGIN_UNIT 3
+#define WIDTH_UNIT 100
+#define HEIGHT_UNIT 10
+#define STATUS_HEIGHT_UNIT 30
+#define FONT_UNIT 5
+#define STATUS_FONT_UNIT 4
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void mouseScrolled(int x, int y, float scroll_x, float scroll_y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void exit();
+
+		//--------------------------------------------------------------
+
+		void updateLayout();
+		void initializeBlockValue();
+		void initializeDrawValue();
+		void guiBlockSetup();
+		void guiDrawSetup();
+		void guiSetListener();
+		void guiSetScale();
+		void guiRebuild();
+
+		void drawObjectUpdate();
+		void blockSettingUpdate();
+		void blockCurrentInfoUpdate();
+		void statusUpdate();
+		void drawStatus();
+		void drawEtc();
+
+		void maxSizeChanged(int & v);
+		void maxBlockCountChanged(int & v);
+		void minBlockCountChanged(int & v);
+		void setBlockClicked();
+		void generateBlockClicked();
+		void drawResetClicked();
+		void saveImageClicked();
+
+		bool checkMouseOnImage();
+		void imageMouseDragged(int x, int y);
+
+		void saveGuiSettings();
+		void loadGuiSettings();
+
+		//--------------------------------------------------------------
+
+		ofTrueTypeFont font_gui;
+		ofTrueTypeFont font_status;
+
+		std::unique_ptr<blockData> block_data;
+		std::unique_ptr<drawObject> draw_object;
+		float gui_width = 400.0f;
+		float gui_height = 40.0f;
+		float margin = 10.0f;
+		float status_height = 120.0f;
+		int image_size = 2000;
+		float gui_scale = 4;
+		bool gui_on = true;
+
+		std::string help_text = "[ Tab ] : Toggle GUI\n[ = ] : GUI scale up\n[ - ] : GUI scale down";
+		float help_height;
+
+		bool generate_request = false;
+		bool frame_delayed = false;
+
+		bool is_image_drag = false;
+		glm::vec2 drag_start_mouse;
+
+		//--------------------------------------------------------------
+
+		ofRectangle rect_draw_gui;
+		ofRectangle rect_block_gui;
+		ofRectangle rect_image;
+		ofRectangle rect_status_block;
+		ofRectangle rect_status_draw;
+
+		ofParameter<int> block_count_1 = 4, block_count_2 = 8, max_r = 3, max_c = 3, max_h = 3;
+		ofParameter<float> density = 20;
+		ofParameter<bool> allow_duplication = false;
+		ofParameter<void> set_block, generate_block;
+		ofParameter<float> cam_degree_xz, cam_degree_y;
+		ofParameter<float> light_degree_xz, light_degree_y;
+		ofParameter<float> magnification;
+		ofParameter<int> thickness;
+		ofParameter<bool> fix_light;
+		ofParameter<ofColor> draw_color;
+		ofParameter<void> save_image, reset;
+		ofParameter<std::string> block_status, draw_status;
+
+		ofParameterGroup block_settings;
+		ofParameterGroup block_generation;
+		ofParameterGroup draw_settings;
+		ofParameterGroup cam_degree;
+		ofParameterGroup light_degree;
+		ofParameterGroup draw_functions;
+
+		float prev_cam_degree_xz;
+		float prev_cam_degree_y;
+};
